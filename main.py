368ffd26e2f3824d740fee3b41a75c2f9f19715b
luizklitzke1 <luiz.klitzke1@gmail.com>
2024-04-27 17:30:38 -0300

    Layout de plot em grid

--- main.py
+++ main.py
@@ -7,59 +7,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    path_datasets = "datasets"
+    path_datasets = "imgs"
     file_names = os.listdir(path_datasets)
 
-    for file_name in file_names:
+    fig, axs = plt.subplots(len(file_names), 6)
+    fig.set_figheight(10)
+    fig.set_figwidth(25)
+
+    for i in range(len(file_names)):
+        file_name = file_names[i]
         img_path = os.path.join(path_datasets, file_name)
 
         img = cv.imread(img_path)
-        plt.title("1- " + file_name)
-        plt.imshow(img)
-        plt.show()
+        axs[i, 0].set_title("1- " + file_name)
+        axs[i, 0].imshow(img)
 
         Z = img.reshape((-1,3))
         Z = np.float32(Z)
 
         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        K = 5
+        K = 4
         ret, label, center = cv.kmeans(Z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
 
         center = np.uint8(center)
         res = center[label.flatten()]
         res2 = res.reshape((img.shape))
-        plt.title("2- K-Means (" + str(K) + ")")
-        plt.imshow(res2)
-        plt.show()
+        axs[i, 1].set_title("2- K-Means (" + str(K) + ")")
+        axs[i, 1].imshow(res2)
 
-        plt.title("3- Grayscale")
+        axs[i, 2].set_title("3- Grayscale")
         img_grayscale = cv.cvtColor(res2, cv.COLOR_BGR2GRAY)
-        plt.imshow(img_grayscale, cmap = "gray")
-        plt.show()
-            
+        axs[i, 2].imshow(img_grayscale, cmap = "gray")
+        
         #Base em https://pyimagesearch.com/2021/04/28/opencv-image-histograms-cv2-calchist/
         hist = cv.calcHist([img_grayscale], [0], None, [256], [0, 256])
-        plt.title('4- Histograma')
-
         #Pega a média do primeiro (mais escuro) tom de cinza no histograma - que representa os núcleos - para separar esse tom
         colors_hist = np.nonzero(hist.flatten())[0]
         first_gray = colors_hist[1]
         second_gray = colors_hist[2]
         lower_threshold = first_gray / 2
         upper_threshold = (first_gray + second_gray) / 2
-        plt.axvline(x = lower_threshold, color = "g", label = "lower_threshold")
-        plt.axvline(x = upper_threshold, color = "r", label = "upper_threshold")
-        plt.legend()
-        plt.plot(hist)
-        plt.show()
 
-        plt.title("5 - Threshold")
+        axs[i, 3].set_title('4- Histograma')
+        axs[i, 3].axvline(x = lower_threshold, color = "g", label = "lower_threshold")
+        axs[i, 3].axvline(x = upper_threshold, color = "r", label = "upper_threshold")
+        axs[i, 3].plot(hist)
+
+
         thresholded_img = cv.inRange(img_grayscale, lower_threshold, upper_threshold)
-        plt.imshow(thresholded_img, cmap = "gray")
-        plt.show()
-    
+        axs[i, 4].set_title("5 - Threshold")
+        axs[i, 4].imshow(thresholded_img, cmap = "gray")
+        
         contours, hierarchy = cv.findContours(thresholded_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         img_contours = cv.drawContours(img, contours, -1, (0, 255, 0), 1)
-        plt.title("6 - Núcleos: " + str(len(contours)))
-        plt.imshow(img_contours)
-        plt.show()
+        axs[i, 5].set_title("6 - Núcleos: " + str(len(contours)))
+        axs[i, 5].imshow(img_contours)
+
+    plt.tight_layout(pad = 0.4, w_pad = 0.5, h_pad = 2)
+    plt.show()
